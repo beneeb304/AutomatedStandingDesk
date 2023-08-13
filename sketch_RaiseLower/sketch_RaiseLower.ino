61e9a32f60ea347ca3b6913d92e4898402d1457d
Ben Neeb <beneeb304@gmail.com>
2023-08-12 23:22:37 -0400

    Added bluetooth serial control

--- sketch_RaiseLower/sketch_RaiseLower.ino
+++ sketch_RaiseLower/sketch_RaiseLower.ino
@@ -1,31 +1,89 @@
-int serialInput;
-int pinRed = 22;
-int pinYellow = 23;
+/*
+Things to still add include:
+- Calibrating time for setting raise/lower intervals
+  - EEPROM or storage
+- Ultrasonic sensor
+*/
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+int strSerialInput;
+int strBTSerialInput;
+int pinRaise = 22;
+int pinLower = 23;
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(pinRed, OUTPUT);
-  pinMode(pinYellow, OUTPUT);
-  digitalWrite(pinRed, HIGH);
-  digitalWrite(pinYellow, HIGH);
+  // Initialize serial
+  Serial.begin(115200);
+  
+  // Configure pins
+  pinMode(pinRaise, OUTPUT);
+  pinMode(pinLower, OUTPUT);
+  digitalWrite(pinRaise, HIGH);
+  digitalWrite(pinLower, HIGH);
+  
+  // Bluetooth initialization
+  SerialBT.begin("SmartDesk_Test"); //Bluetooth device name
+  Serial.println("The device started, now you can connect it with bluetooth!");
 }
 
 void loop()
 {
-  serialInput = Serial.read();
-  if(serialInput == 'R')
+  // Read any serial inputs
+  strSerialInput = Serial.read();
+  strBTSerialInput = SerialBT.read();
+  
+  // Check for serial triggers
+  switch (strSerialInput)
   {
-    Serial.println("Red Pin");
-    digitalWrite(pinRed, LOW);
-    delay(2000);
-    digitalWrite(pinRed, HIGH);
-  } 
-  else if (serialInput == 'Y')
-  {
-    Serial.println("Yellow Pin");
-    digitalWrite(pinYellow, LOW);
-    delay(2000);
-    digitalWrite(pinYellow, HIGH);
+    case 'R':
+      Serial.println("Raise for 2 seconds...");
+      digitalWrite(pinRaise, LOW);
+      delay(2000);
+      digitalWrite(pinRaise, HIGH);
+      break;
+    
+    case 'L':
+      Serial.println("Lower for 2 seconds...");
+      digitalWrite(pinLower, LOW);
+      delay(2000);
+      digitalWrite(pinLower, HIGH);
+      break;
+    
+    default:
+      break;
   }
+
+  // Check for bluetooth serial triggers
+  switch (strBTSerialInput)
+  {
+  case 'R':
+      SerialBT.println("Raise for 2 seconds...");
+      digitalWrite(pinRaise, LOW);
+      delay(2000);
+      digitalWrite(pinRaise, HIGH);
+      break;
+    break;
+  
+  case 'L':
+      Serial.println("Lower for 2 seconds...");
+      digitalWrite(pinLower, LOW);
+      delay(2000);
+      digitalWrite(pinLower, HIGH);
+      break;
+    break;
+  
+  default:    
+    // Could check for calibration values here?
+    break;
+  }
+  
+  // Minor delay to avoid hiccups
+  delay(20);
 }
